33a8afa71c126538ab0ec78518f07209f95182b2
daniel <kigdaniel04@gmail.com>
2023-06-22 22:13:22 +0300

    added comments

--- script.py
+++ script.py
@@ -3,28 +3,29 @@ from abis import *
 from web3 import Web3
 import time
 
-f = open('config.json')
-config = json.load(f)
-
-# function checking providers
+config = json.load(open('config.json'))
 
 
+# checking providers
 def check_provider(tlos_providers):
     for provider in tlos_providers:
         if tlos_provider := Web3(Web3.HTTPProvider(provider)):
             return tlos_provider
 
 
+# retrievs the usdgamount 
 def getSizes(data, config):
-    all = [data[2], data[17], data[32]]
-    return [x * config for x in all]
+    targetSize = [data[2], data[17], data[32]]
+    return [x * config for x in targetSize]
 
 
+# calculates the deviation
 def deviation(sizeType, maxGlobalSize):
-    all = []
+    deviations = []
     for x in range(3):
-        all.append(abs((sizeType[x] - maxGlobalSize[x]) / maxGlobalSize[x]) * 100)
+        deviations.append(abs((sizeType[x] - maxGlobalSize[x]) / maxGlobalSize[x]) * 100)
     return all
+
 
 def main():
     tlos_provider = check_provider(config["telos_mainnet"])
@@ -37,13 +38,15 @@ def main():
                                                   0,
                                                   config['volatileTokens']
                                                   ).call()
-
+    # calculating target sizes
     targetShortSize = getSizes(data, config["SHORT_SIZE_POOL_RATIO"])
     targetLongSize = getSizes(data, config["LONG_SIZE_POOL_RATIO"])
 
+    # getting max global sizes from the contract
     maxGlobalShortSize = [data[8], data[23], data[38]]
     maxGlobalLongSize = [data[9], data[24], data[39]]
 
+    # calculating deviations
     shortDeviation = deviation(targetShortSize, maxGlobalShortSize)
     longDeviation = deviation(targetLongSize, maxGlobalLongSize)
 
